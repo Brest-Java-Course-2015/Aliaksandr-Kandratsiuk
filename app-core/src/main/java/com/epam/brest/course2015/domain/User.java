7e83e72aaea259d18f1611a897e65fc351b50716
AlexanderK <ak.brest@gmail.com>
2015-10-15 23:03:33 +0300

    updatedDate all methods dao

--- app-core/src/main/java/com/epam/brest/course2015/domain/User.java
+++ app-core/src/main/java/com/epam/brest/course2015/domain/User.java
@@ -15,6 +15,20 @@ public class User {
 
     private Date createdDate;
 
+    private Date updatedDate;
+
+    public User() {
+    }
+
+    public User(Integer userId, String login, String password, Date createdDate, Date updatedDate) {
+        this.userId = userId;
+        this.login = login;
+        this.password = password;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+    }
+
+
     public Integer getUserId() {
         return userId;
     }
@@ -45,5 +59,28 @@ public class User {
 
     public void setCreatedDate(Date createdDate) {
         this.createdDate = createdDate;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public static enum UserFields {
+        USER_ID ("userId"),
+        LOGIN ("login"),
+        PASSWORD ("password"),
+        CREATED_DATE ("createdDate"),
+        UPDATED_DATE ("updatedDate");;
+
+        private UserFields(String value){
+            this.value = value;
+        }
+
+        private final String value;
+        public String getValue(){return value;}
     }
 }
--- app-dao-api/src/main/java/com/epam/brest/course2015/dao/UserDao.java
+++ app-dao-api/src/main/java/com/epam/brest/course2015/dao/UserDao.java
@@ -8,11 +8,10 @@ import java.util.List;
  * Created by juga on 7.10.15.
  */
 public interface UserDao {
-
     public List<User> getAllUsers();
-
-    public User getUserById(Integer id);
-
-    public void deleteUser(Integer id);
-
+    public User getUserById(Integer userId);
+    public User getUserByLogin(String login);
+    public Integer addUser(User user);
+    public void updateUser(User user);
+    public void deleteUser(Integer userId);
 }
